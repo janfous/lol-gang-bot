0651617cc0a2019feb130425640954fd97f30777
JAN FOUS <jan.fous@ateli.cz>
2021-03-19 10:48:35 +0100

    LGB-0.1.1-initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# lol-gang-bot
+Discord bot for LOL Gang
+
+#v0.1.1
+
+###Based counter (WIP)
+
+Finished: counter, printing  
+In progress: leaderboard
+
---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require('Discord.js');
+const env = require('dotenv');
+const fs = require('fs');
+
+env.config();
+
+const prefix = process.env.PREFIX;
+const token = process.env.TOKEN;
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const command_folder = fs.readdirSync("./commands");
+
+for (const folder of command_folder) {
+    const command_files = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of command_files) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
+}
+
+
+client.login(token);
+
+client.on("message", message => {
+
+    if (message.author.bot) {
+        return;
+    }
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case `based`:
+            client.commands.get("based").execute(message, args);
+            break;
+    }
+
+});
+
